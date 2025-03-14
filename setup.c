ce71628ecb67a9dca799be6cc6836d86035af5c2
taku-256 <146168304+taku-256@users.noreply.github.com>
2025-03-14 16:41:00 +0900

    add pin-assign

--- setup.c
+++ setup.c
@@ -20,6 +20,7 @@
 void setup(){
     OSC_set();
     pin_set();
+//    CN_setup();
 //    UART1_setup(115200);
 //    UART2_setup(115200);
 //    TMR1_setup();
@@ -35,7 +36,6 @@ void OSC_set(){
     ACLKDIV3 = 0x0007;
 }
 
-/* TODO : Check pin-assign */
 void pin_set(){
     ANSELA = 0x0000;
     ANSELB = 0x0000;
@@ -56,27 +56,124 @@ void pin_set(){
     TRISG = 0xffff;
     
 //  Main Board | UART1
-//    TRIS  = 0;
-//    RPOR = 0b000001;
-//    TRIS  = 1;
-//    RPINR18bits.U1RXR = ;
+    TRISGbits.TRISG13 = 0;
+    RPOR14bits.RP125R = 0b000001;
+    TRISGbits.TRISG12 = 1;
+    RPINR18bits.U1RXR = 124;
     
 //  Debug | UART2
-//    TRIS  = 0;
-//    RPOR = 0b000011;
-//    TRIS  = 1;
-//    RPINR19bits.U2RXR = ;
-    
+    TRISEbits.TRISE4  = 0;
+    RPOR5bits.RP84R   = 0b000011;
+    TRISEbits.TRISE3  = 1;
+    RPINR19bits.U2RXR = 183;    
 
 // Buzzer | OC1
-//    TRIS
-//    RPOR = 0b010000;
+    TRISFbits.TRISF8  = 0;
+    RPOR11bits.RP104R = 0b010000;
+    
+// LED
+    TRISFbits.TRISF0 = 0; // LED1
+    TRISFbits.TRISF1 = 0; // LED2
+    TRISGbits.TRISG1 = 0; // LED3
     
 // LINE
+    /* Right */
+    TRISDbits.TRISD7  = 1; // 1 front
+    TRISDbits.TRISD6  = 1; // 2
+    TRISDbits.TRISD5  = 1; // 3
+    TRISDbits.TRISD4  = 1; // 4
+    TRISDbits.TRISD13 = 1; // 5 back
     
-//    TRIS  = 1; 
+    /* Back */
+    TRISDbits.TRISD0  = 1; // 1 Right
+    TRISDbits.TRISD11 = 1; // 2
+    TRISDbits.TRISD10 = 1; // 3
+    TRISDbits.TRISD9  = 1; // 4
+    TRISDbits.TRISD8  = 1; // 5 Left
     
-//    CNEN  = 1;
+    /* Left */
+    TRISBbits.TRISB15 = 1; // 1 back
+    TRISBbits.TRISB14 = 1; // 2
+    TRISBbits.TRISB13 = 1; // 3
+    TRISBbits.TRISB12 = 1; // 4
+    TRISFbits.TRISF12 = 1; // 5 front
+    
+    /* Angel Ring */
+    TRISDbits.TRISD12 = 1; //  1-1
+    TRISDbits.TRISD3  = 1; //  1-2
+    TRISDbits.TRISD2  = 1; //  2-1
+    TRISDbits.TRISD1  = 1; //  2-2
+    TRISAbits.TRISA15 = 1; //  3-1
+    TRISAbits.TRISA14 = 1; //  3-2
+    TRISAbits.TRISA5  = 1; //  4-1
+    TRISAbits.TRISA4  = 1; //  4-2
+    TRISAbits.TRISA3  = 1; //  5-1
+    TRISAbits.TRISA2  = 1; //  5-2
+    TRISFbits.TRISF2  = 1; //  6-1
+    TRISFbits.TRISF3  = 1; //  6-2
+    TRISFbits.TRISF5  = 1; //  7-1
+    TRISFbits.TRISF4  = 1; //  7-2
+    TRISDbits.TRISD15 = 1; //  8-1
+    TRISDbits.TRISD14 = 1; //  8-2
+    TRISFbits.TRISF13 = 1; //  9-1
+    TRISAbits.TRISA1  = 1; //  9-2
+    TRISBbits.TRISB10 = 1; // 10-1
+    TRISBbits.TRISB9  = 1; // 10-2
+    TRISBbits.TRISB3  = 1; // 11-1
+    TRISBbits.TRISB4  = 1; // 11-2
+    TRISBbits.TRISB5  = 1; // 12-1
+    TRISEbits.TRISE9  = 1; // 12-2
+    TRISCbits.TRISC3  = 1; // 13-1
+    TRISCbits.TRISC2  = 1; // 13-2
+    TRISCbits.TRISC1  = 1; // 14-1
+    TRISEbits.TRISE7  = 1; // 14-2
+    
+}
+
+void CN_setup(){
+    CNENDbits.CNIED7  = 1;
+    CNENDbits.CNIED6  = 1;
+    CNENDbits.CNIED5  = 1;
+    CNENDbits.CNIED4  = 1;
+    CNENDbits.CNIED13 = 1;
+    CNENDbits.CNIED0  = 1;
+    CNENDbits.CNIED11 = 1;
+    CNENDbits.CNIED10 = 1;
+    CNENDbits.CNIED9  = 1;
+    CNENDbits.CNIED8  = 1;
+    CNENBbits.CNIEB15 = 1;
+    CNENBbits.CNIEB14 = 1;
+    CNENBbits.CNIEB13 = 1;
+    CNENBbits.CNIEB12 = 1;
+    CNENFbits.CNIEF12 = 1;
+    CNENDbits.CNIED12 = 1;
+    CNENDbits.CNIED3  = 1;
+    CNENDbits.CNIED2  = 1;
+    CNENDbits.CNIED1  = 1;
+    CNENAbits.CNIEA15 = 1;
+    CNENAbits.CNIEA14 = 1;
+    CNENAbits.CNIEA5  = 1;
+    CNENAbits.CNIEA4  = 1;
+    CNENAbits.CNIEA3  = 1;
+    CNENAbits.CNIEA2  = 1;
+    CNENFbits.CNIEF2  = 1;
+    CNENFbits.CNIEF3  = 1;
+    CNENFbits.CNIEF5  = 1;
+    CNENFbits.CNIEF4  = 1;
+    CNENDbits.CNIED15 = 1;
+    CNENDbits.CNIED14 = 1;
+    CNENFbits.CNIEF13 = 1;
+    CNENAbits.CNIEA1  = 1;
+    CNENBbits.CNIEB10 = 1;
+    CNENBbits.CNIEB9  = 1;
+    CNENBbits.CNIEB3  = 1;
+    CNENBbits.CNIEB4  = 1;
+    CNENBbits.CNIEB5  = 1;
+    CNENEbits.CNIEE9  = 1;
+    CNENCbits.CNIEC3  = 1;
+    CNENCbits.CNIEC2  = 1;
+    CNENCbits.CNIEC1  = 1;
+    CNENEbits.CNIEE7  = 1;
     
     IEC1bits.CNIE = 1;
     IPC4bits.CNIP = 2;
